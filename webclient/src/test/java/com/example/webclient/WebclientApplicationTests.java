4f2a90abe1002fb4be89e5a908183e03dbbff0f2
elyudwo <dudqk9696@naver.com>
2024-04-15 22:33:26 +0900

    [Feat] : CI/CD 구축 2

--- webclient/src/test/java/com/example/webclient/WebclientApplicationTests.java
+++ webclient/src/test/java/com/example/webclient/WebclientApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.webclient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class WebclientApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class WebclientApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
